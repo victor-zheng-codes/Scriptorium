724c3978c5db1c1e019fe3eee8a74ebdea3af60b
Abdullah Safi <abdullah.safi@mail.utoronto.ca>
2024-11-27 21:41:25 -0500

    feat: make blogs search background like templates

--- PP2/pages/blogs/index.tsx
+++ PP2/pages/blogs/index.tsx
@@ -170,7 +170,7 @@ const Blogs = () => {
   return (
     <Layout>
       <h1 className="text-4xl text-center font-bold py-8 dark:text-gray-200">Blogs</h1>
-      <div className="py-4 px-4 sm:px-6 md:px-16 lg:px-32 space-y-4">
+      <div className="container py-4 px-4 sm:px-6 md:px-16 lg:px-32 space-y-4">
         <form onSubmit={handleSearch} className="space-y-4">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 text-gray-600 dark:text-gray-300">
             <input
@@ -250,7 +250,7 @@ const Blogs = () => {
             </div>
           ) : (
           blogs.map((blog) => (
-            <div key={blog.blogId} className="p-4 border border-gray-500 rounded-md text-gray-600 dark:text-gray-300">
+            <div key={blog.blogId} className="mb-8 p-6 bg-white dark:bg-gray-925 rounded-lg shadow-md">
               <h2 className="text-xl font-bold">
                 <Link href={`/blogs/${blog.blogId}`} className="text-blue-500 hover:underline">
                   {blog.title}
